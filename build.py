bfc5e7e0178d3cb26a6f94e9a60306b361a98fdc
adam nunez <adam.a.nunez@gmail.com>
2017-07-12 10:00:26 -0500

    Check that pandoc is installed before anyting else.

--- build.py
+++ build.py
@@ -10,11 +10,18 @@ POSTS_DIR = 'posts'
 TEMPLATE = 'template.html'
 FLAG = '#CONTENT'
 
+print('Checking for pandoc.')
+if os.system('pandoc isPandocInstalled') != 256:
+    print('Please insatll pandoc')
+    raise SystemExit
+
+print('Removing old stuff.')
 try: os.remove(MAIN)
 except: pass
 try: os.remove(BUILD_FILE)
 except: pass
 
+print('Making the new stuff.')
 mdlist = next(os.walk(POSTS_DIR))[2]
 list.sort(mdlist, reverse=True)
 mdlist = ' posts/'.join(mdlist)
@@ -30,13 +37,19 @@ with open(MAIN, 'r+') as indexfh:
     indexfh.seek(pos)
     with open(BUILD_FILE, 'r') as buildfh:
         for line in buildfh:
+
+            # Find the titles and adjust the size of the date.
             if line.startswith('<h2') and (line.find('|') != -1):
                 work = line.split('|')
                 date = '<span class="date">' + work[1].split('<')[0] + '</span></h2>'
                 indexfh.write(work[0] + date)
             else:
                 indexfh.write(line)
+
     indexfh.write(index_footer)
 
+print('Cleaning up')
 try: os.remove(BUILD_FILE)
 except: pass
+
+print('Done')
