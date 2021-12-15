bd493bc9b3c5652ed59dff042c629ef35d5f28c7
adam nunez <adam.a.nunez@gmail.com>
2021-12-15 14:09:51 -0600

    Adding all CSS to static

--- .eleventy.js
+++ .eleventy.js
@@ -33,10 +33,12 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
     "./src/admin/config.yml": "./admin/config.yml",
     "./node_modules/alpinejs/dist/cdn.min.js": "./static/js/alpine.js",
-    "./node_modules/prismjs/themes/prism-tomorrow.css":
-      "./static/css/prism-tomorrow.css",
+    "./node_modules/prismjs/themes/prism-tomorrow.css": "./static/css/prism-tomorrow.css",
   });
 
+  // Copy CSS Folder to /_site
+  eleventyConfig.addPassthroughCopy("./src/static/css");
+  
   // Copy Image Folder to /_site
   eleventyConfig.addPassthroughCopy("./src/static/img");
 
