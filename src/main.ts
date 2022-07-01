9cb6d5adf6b8c9f4df34b1299f6e2263233550bb
adam nunez <adam.a.nunez@gmail.com>
2022-06-30 20:07:08 -0500

    Missing a '/' that broke everythig

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import './style.css'
 
-let email = document.getElementById("email") as HTMLElement
+const email = document.getElementById("email") as HTMLElement
 email.setAttribute("href", "mailto:adam.a.nunez@gmail.com")
 
-let linkedin = document.getElementById("linkedin") as HTMLElement
+const linkedin = document.getElementById("linkedin") as HTMLElement
 linkedin.setAttribute("href", "https://www.linkedin.com/in/adam-a-nunez/")
 
-let github = document.getElementById("github") as HTMLElement
+const github = document.getElementById("github") as HTMLElement
 github.setAttribute("href", "https://github.com/aanunez/");
