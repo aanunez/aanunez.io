e38dbd0c958a39d20098d0c4f827994780853942
adam nunez <adam.a.nunez@gmail.com>
2024-05-16 16:06:52 -0700

    Update site

--- main.ts
+++ main.ts
@@ -15,6 +15,8 @@ emrepo.setAttribute("href", "https://redcap.vanderbilt.edu/consortium/modules/in
 const uwctri = document.getElementById("uwctri") as HTMLElement
 uwctri.setAttribute("href", "https://github.com/uwctri")
 
-const precalc = "0,100 20,96 40,93 60,86 80,100 100,93 120,100 140,93 160,90 180,93 200,73 220,80 240,90 260,73 280,66 300,80 320,96 340,96 360,86 380,66 400,93 420,90 440,93 460,93 480,93 500,86 520,100 540,100 560,100 580,50"
+
+
+const precalc = "0,100 20,86 40,100 60,93 80,100 100,93 120,90 140,93 160,73 180,80 200,90 220,73 240,66 260,80 280,96 300,96 320,86 340,66 360,93 380,90 400,93 420,93 440,93 460,86 480,100 500,100 520,100 540,50 560,80 580,96"
 const ghline = document.getElementById("ghline") as HTMLElement
 ghline.setAttribute("points", precalc)
