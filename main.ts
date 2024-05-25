33361c99ee14655ba489fd001cc8665e1cc044c5
adam nunez <adam.a.nunez@gmail.com>
2024-05-24 17:35:21 -0700

    Add prebuild step

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import './style.css'
+import line from './plot.json'
 
 const email = document.getElementById("email") as HTMLElement
 email.setAttribute("href", "mailto:adam.a.nunez@gmail.com")
@@ -15,8 +16,5 @@ emrepo.setAttribute("href", "https://redcap.vanderbilt.edu/consortium/modules/in
 const uwctri = document.getElementById("uwctri") as HTMLElement
 uwctri.setAttribute("href", "https://github.com/uwctri")
 
-
-
-const precalc = "0,100 20,86 40,100 60,93 80,100 100,93 120,90 140,93 160,73 180,80 200,90 220,73 240,66 260,80 280,96 300,96 320,86 340,66 360,93 380,90 400,93 420,93 440,93 460,86 480,100 500,100 520,100 540,50 560,80 580,96"
 const ghline = document.getElementById("ghline") as HTMLElement
-ghline.setAttribute("points", precalc)
+ghline.setAttribute("points", line.data)
---
+++ prebuild.js
@@ -0,0 +1,57 @@
+fs = require('node:fs')
+const token = process.env.GITHUB_TOKEN
+const user = "aanunez"
+const size = 100
+const search = "https://api.github.com/search/commits?q="
+
+let page = 1
+let total = 9999
+let history = new Proxy({}, {
+    get: function (target, name) {
+        return target.hasOwnProperty(name) ? target[name] : 0
+    }
+})
+
+let today = new Date()
+let date = new Date(new Date().setDate(today.getDate() - 30))
+let ymd = date.toISOString().split('T')[0]
+const todayYMD = today.toISOString().split('T')[0]
+    ;
+(async () => {
+    while ((size * page) <= (Math.ceil(total / size) * size)) {
+        await fetch(`${search}author:${user}+author-date:>${ymd}&per_page=${size}&page=${page}`, {
+            headers: {
+                "Accept": "application/vnd.github+json",
+                "Authorization": `Bearer ${token}`,
+                "X-GitHub-Api-Version": "2022-11-28"
+            }
+        }).then(res => res.json()).then(data => {
+            total = data.total_count
+            for (entry of data.items) {
+                history[entry.commit.author.date.split("T")[0]] += 1
+            }
+            page += 1
+        })
+    }
+
+    let scale = 100 / Math.max(...Object.values(history))
+    scale = scale > 1 ? scale * .5 : scale
+    let svgplot = []
+    let score = 0
+    let count = 0
+    while (ymd < todayYMD) {
+        date = (new Date(new Date(date).setDate(date.getDate() + 1)))
+        ymd = date.toISOString().split('T')[0]
+        score = Math.floor(100 - (history[ymd] * scale))
+        svgplot.push(`${20 * count},${score}`)
+        count += 1
+    }
+
+    try {
+        fs.writeFileSync("plot.json", JSON.stringify({
+            data: svgplot.join(" ")
+        }))
+    } catch (err) {
+        console.error(err)
+    }
+})()
